ba29fb2d08a95f5e59cb6f316e58fbab222cd19a
MNL3 <136118418+MNL3@users.noreply.github.com>
2023-07-10 20:07:42 +0300

    Add files via upload

---
+++ data_create.py
@@ -0,0 +1,19 @@
+def name_data():
+    name = input('Введите имя контакта: ')
+    return name
+
+
+def surname_data():
+    surname = input('Введите фамилию контакта: ')
+    return surname
+
+
+def phone_data():
+    # import re
+    phone = input('Введите номер телефона контакта: ')
+    return phone
+
+
+def adress_data():
+    adress = input('Введите адрес контакта: ')
+    return adress
